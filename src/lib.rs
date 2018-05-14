ce54d25ce5d975d8b11d00dff541b542d32d4057
Julian Baehr <julian.baehr@googlemail.com>
2018-05-14 21:37:55 +0200

    added needed dependencies
    bitflags, byteorder, bitfield, etc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(try_from)]
+
 #[macro_use]
 extern crate getset;
 
@@ -5,6 +7,15 @@ extern crate getset;
 extern crate error_chain;
 #[macro_use]
 extern crate log;
+
+#[macro_use]
+extern crate enum_primitive_derive;
+extern crate num_traits;
+extern crate byteorder;
+#[macro_use]
+extern crate bitflags;
+#[macro_use]
+extern crate bitfield;
 
 pub mod ctl;
 pub mod ds;
