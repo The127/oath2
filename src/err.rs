0ffa5dd698bfc2de96a1ff0d8ea1bc9569cd6526
Julian Baehr <julian.baehr@googlemail.com>
2018-05-14 22:00:16 +0200

    added error kinds

--- src/err.rs
+++ src/err.rs
@@ -2,13 +2,24 @@ error_chain!{
     types{
         Error, ErrorKind, ResultExt, Result;
     }
+
     links {
 
     }
+
     foreign_links{
         Io(::std::io::Error);
     }
-    errors{
 
+    errors{
+        InvalidSliceLength(expected: usize, actual: usize, ttype: &'static str) {
+            description("Invalid slice length received."),
+            display("Expected '{}' but got '{}' bytes for '{}'.", expected, actual, ttype),
+        }
+
+        UnknownValue(val: u64, ttype: &'static str) {
+            description("Encountered unknown value."),
+            display("Encountered unknown value '{}' for type '{}.", val, ttype),
+        }
     }
 }
